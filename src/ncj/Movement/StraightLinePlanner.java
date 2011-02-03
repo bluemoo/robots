e19b44fc243baa8488a61b5f04f134af67fb6f84
Noah <noah@motivation.(none)>
2011-02-03 16:16:24 -0700

    Added a slight curvature to the straight line planner. Rename to either direction planner?

--- src/ncj/Movement/StraightLinePlanner.java
+++ src/ncj/Movement/StraightLinePlanner.java
@@ -3,6 +3,8 @@ package ncj.Movement;
 import java.util.ArrayList;
 
 import ncj.EnemyAnalysis;
+import ncj.IGearbox;
+import ncj.Vector2D;
 import ncj.Wave;
 
 public class StraightLinePlanner extends ThreeSixtyMovementPlanner {
@@ -16,11 +18,45 @@ public class StraightLinePlanner extends ThreeSixtyMovementPlanner {
 	public ArrayList<MovementPlan> calculatePlansToTry(Wave wave) {
 		ArrayList<MovementPlan> plans = new ArrayList<MovementPlan>();
 		
-		for(double d = -600; d<=600; d+=3)
+		double rot1 = calculateRotation1(wave, _movementController.getCurrentGearbox());
+		double rot2 = calculateRotation2(wave, _movementController.getCurrentGearbox());
+		
+		for(double d = 0; d<=600; d+=3)
 		{
-			plans.add(new MovementPlan().setTurn(0).setAhead(d));
+			plans.add(new MovementPlan().setTurn(rot1).setAhead(d));
 		}
+		
+		for(double d = 0; d>=-600; d-=3)
+		{
+			plans.add(new MovementPlan().setTurn(rot2).setAhead(d));
+		}
+		
 		return plans;
+	}
+	
+	private double calculateRotation1(Wave wave, IGearbox gearbox) {
+		Vector2D pRobot = new Vector2D(gearbox.getX(), gearbox.getY());
+		Vector2D pWave = new Vector2D(wave.getX(), wave.getY());
+		Vector2D vWtoR = pRobot.minus(pWave);
+		Vector2D vDesired = vWtoR.calculatePerpendicular();
+		double bearing = Math.PI/2 - Math.atan2(vDesired.getY(), vDesired.getX());
+		double rotation = robocode.util.Utils.normalRelativeAngle(bearing - gearbox.getHeadingRadians());
+		
+		if( Math.abs(rotation) > Math.PI/2)
+			rotation = robocode.util.Utils.normalRelativeAngle(rotation + Math.PI);
+		return rotation;
+	}
+	private double calculateRotation2(Wave wave, IGearbox gearbox) {
+		Vector2D pRobot = new Vector2D(gearbox.getX(), gearbox.getY());
+		Vector2D pWave = new Vector2D(wave.getX(), wave.getY());
+		Vector2D vWtoR = pRobot.minus(pWave);
+		Vector2D vDesired = vWtoR.calculatePerpendicular();
+		double bearing = Math.PI/2 - Math.atan2(vDesired.getY(), vDesired.getX()) + Math.PI;
+		double rotation = robocode.util.Utils.normalRelativeAngle(bearing - gearbox.getHeadingRadians());
+		
+		if( Math.abs(rotation) > Math.PI/2)
+			rotation = robocode.util.Utils.normalRelativeAngle(rotation + Math.PI);
+		return rotation;
 	}
 
 }
